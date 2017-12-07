f3091d37c7f3f06f70158ea6142232a1af7c5c8c
dannymoon <moon.hojik@gmail.com>
2017-12-07 14:17:35 -0800

    done

---
+++ cat.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    
+    $("#ninja0").click(function(){
+        $('#ninja0').attr('src','./img/cat0.png');   
+   });
+   $("#ninja1").click(function(){
+    $('#ninja1').attr('src','./img/cat1.png');   
+    });
+    $("#ninja2").click(function(){
+        $('#ninja2').attr('src','./img/cat2.png');   
+   });
+   $("#ninja3").click(function(){
+    $('#ninja3').attr('src','./img/cat3.png');   
+    });
+    $("#ninja4").click(function(){
+        $('#ninja4').attr('src','./img/cat4.png');   
+   });
+});
+
